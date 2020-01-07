95c6462e7fcf4f26cba17c3b5c6b5676e2fb70e1
Akito <akito.kitsune@protonmail.com>
2020-01-07 20:50:15 +0100

    Better UX + Better LICENCE Info

--- scripts/cleanup_rancher.sh
+++ scripts/cleanup_rancher.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
-# See LICENSE.
-# Copyright (C) 2019 Akito <akito.kitsune@protonmail.com>
+#########################################################################
+# Copyright (C) 2019-2020 Akito <the@akito.ooo>                         #
+#                                                                       #
+# This program is free software: you can redistribute it and/or modify  #
+# it under the terms of the GNU General Public License as published by  #
+# the Free Software Foundation, either version 3 of the License, or     #
+# (at your option) any later version.                                   #
+#                                                                       #
+# This program is distributed in the hope that it will be useful,       #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          #
+# GNU General Public License for more details.                          #
+#                                                                       #
+# You should have received a copy of the GNU General Public License     #
+# along with this program. If not, see <http://www.gnu.org/licenses/>.  #
+#########################################################################
 # Based on
 # https://github.com/rancher/rancher/issues/19882#issuecomment-501056386
 
@@ -51,7 +65,7 @@ function docker_restart {
   else
     echoError "Docker restart failed!"
     white_printf "Manually stop docker then wait 20 seconds and start it again.\n"
-    return 1
+    exit 1
   fi
 }
 function rmContainers {
@@ -147,7 +161,7 @@ cleanFirewall
 # Restarts services, to apply previous removals.
 containerd_restart
 # Slowed down Docker restart. Needs a pause, because else it complains about "too quick" restarts.
-docker_restart && \
+docker_restart
 # Everything went smoothly; process finished.
 finish_line
 
