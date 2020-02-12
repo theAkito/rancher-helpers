b4a7a1404c483b90a69e8617927c3c7e73ecd470
Akito <the@akito.ooo>
2020-02-13 00:31:29 +0100

    Added "debian-buster_fix.sh"

---
+++ scripts/debian-buster_fix.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#########################################################################
+# Copyright (C) 2020 Akito <the@akito.ooo>                              #
+#                                                                       #
+# This program is free software: you can redistribute it and/or modify  #
+# it under the terms of the GNU General Public License as published by  #
+# the Free Software Foundation, either version 3 of the License, or     #
+# (at your option) any later version.                                   #
+#                                                                       #
+# This program is distributed in the hope that it will be useful,       #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          #
+# GNU General Public License for more details.                          #
+#                                                                       #
+# You should have received a copy of the GNU General Public License     #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>. #
+#########################################################################
+# Based on https://github.com/coredns/coredns/issues/2693#issuecomment-543208513
+#################################   Boilerplate of the Boilerplate   ####################################################
+# Coloured Echoes                                                                                                       #
+function red_echo      { echo -e "\033[31m$@\033[0m";   }                                                               #
+function green_echo    { echo -e "\033[32m$@\033[0m";   }                                                               #
+function yellow_echo   { echo -e "\033[33m$@\033[0m";   }                                                               #
+function white_echo    { echo -e "\033[1;37m$@\033[0m"; }                                                               #
+# Coloured Printfs                                                                                                      #
+function red_printf    { printf "\033[31m$@\033[0m";    }                                                               #
+function green_printf  { printf "\033[32m$@\033[0m";    }                                                               #
+function yellow_printf { printf "\033[33m$@\033[0m";    }                                                               #
+function white_printf  { printf "\033[1;37m$@\033[0m";  }                                                               #
+# Debugging Outputs                                                                                                     #
+function white_brackets { local args="$@"; white_printf "["; printf "${args}"; white_printf "]"; }                      #
+function echoInfo   { local args="$@"; white_brackets $(green_printf "INFO") && echo " ${args}"; }                      #
+function echoWarn   { local args="$@";  echo "$(white_brackets "$(yellow_printf "WARN")" && echo " ${args}";)" 1>&2; }  #
+function echoError  { local args="$@"; echo "$(white_brackets "$(red_printf    "ERROR")" && echo " ${args}";)" 1>&2; }  #
+# Silences commands' STDOUT as well as STDERR.                                                                          #
+function silence { local args="$@"; ${args} &>/dev/null; }                                                              #
+# Check your privilege.                                                                                                 #
+function checkPriv { if [[ "$EUID" != 0 ]]; then echoError "Please run me as root."; exit 1; fi;  }                     #
+# Returns 0 if script is sourced, returns 1 if script is run in a subshell.                                             #
+function checkSrc { (return 0 2>/dev/null); if [[ "$?" == 0 ]]; then return 0; else return 1; fi; }                     #
+# Prints directory the script is run from. Useful for local imports of BASH modules.                                    #
+# This only works if this function is defined in the actual script. So copy pasting is needed.                          #
+function whereAmI { printf "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )";   }                     #
+# Alternatively, this alias works in the sourcing script, but you need to enable alias expansion.                       #
+alias whereIsMe='printf "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"'                            #
+#########################################################################################################################
+checkPriv
+targets=( "iptables /usr/sbin/iptables-legacy" \
+          "ip6tables /usr/sbin/ip6tables-legacy" \
+          "arptables /usr/sbin/arptables-legacy" \
+          "ebtables /usr/sbin/ebtables-legacy" \
+        )
+for target in "${targets[@]}"; do
+  first_column="$(echo ${target} | awk '{print $1}')"
+  second_column="$(echo ${target} | awk '{print $2}')"
+  silence "update-alternatives --set ${target}"
+  if [[ $? ]]; then
+    echoInfo "Successfully set \"${first_column}\" to \"${second_column}\"."
+  else
+    echoInfo "Omitted setting \"${first_column}\" to \"${second_column}\"."
+  fi
+done
